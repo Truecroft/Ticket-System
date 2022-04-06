bdb71eafcabe64ee9f20d20af409ebb6a468a565
Tom Ruecroft <thomas.ruecroft1@nhs.net>
2022-04-06 10:41:08 +0100

    Change RadioField to SelectField

--- ticket_website/admin/admin_forms.py
+++ ticket_website/admin/admin_forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from sqlalchemy import false, true
-from wtforms import StringField, IntegerField, TextAreaField, RadioField, PasswordField, SelectField
+from wtforms import StringField, IntegerField, TextAreaField, PasswordField, SelectField
 from wtforms.validators import DataRequired, Email, EqualTo
 
 class CreateUser(FlaskForm):
@@ -13,7 +13,7 @@ class CreateUser(FlaskForm):
         validators=[DataRequired(), EqualTo('confirm_password', message='Passwords Must Match')], 
         render_kw={"placeholder": "Confirm Password"})
     
-    user_status = RadioField('Is this User an Admin', choices=[(True, 'Yes'), (False,'No')])
+    user_status = SelectField('Is this User an Admin', choices=[(True, 'Admin User'), (False, 'Regular User')], default=[False])
 
 
 class EditTicket(FlaskForm):
@@ -29,4 +29,4 @@ class EditUser(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()], render_kw={"placeholder": "john@doe.com"})
     first_name = StringField('First Name', validators=[DataRequired()], render_kw={"placeholder": "John"})
     last_name = StringField('Last Name', validators=[DataRequired()], render_kw={"placeholder": "Doe"})    
-    user_status = RadioField('Is this User an Admin', choices=[(True, 'Yes'), (False,'No')], default=False)
+    user_status = SelectField('Is this User an Admin', choices=[(True, 'Admin User'), (False, 'Regular User')], default=[False])
--- ticket_website/tickets/ticket_forms.py
+++ ticket_website/tickets/ticket_forms.py
@@ -7,11 +7,9 @@ class CreateTicket(FlaskForm):
     description = TextAreaField('Description', validators=[DataRequired()])
     contact_number = IntegerField('Contact Number', validators=[DataRequired()])
     affected_item = StringField('Affected Item', validators=[DataRequired()])
-    # status = SelectField('Programming Language', choices=[('open', 'Open'), ('resolved', 'Resolved'), ('closed', 'Closed')])
 
 class EditTicket(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     description = TextAreaField('Description', validators=[DataRequired()])
     contact_number = IntegerField('Contact Number', validators=[DataRequired()])
     affected_item = StringField('Affected Item', validators=[DataRequired()])
-    # status = SelectField('Programming Language', choices=[('open', 'Open'), ('resolved', 'Resolved'), ('closed', 'Closed')])
